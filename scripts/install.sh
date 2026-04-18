61ef1ab9cd4be573eabef5c504bc6f6e087c3542
mike <vatolkinmihajlo@gmail.com>
2026-04-19 01:47:56 +0300

    update install, add -y to apt-get

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,7 +21,7 @@ EOF
 apt update
 apt upgrade -y
 apt install -y nginx nodejs npm postgresql docker.io
-apt-get install docker-compose-plugin
+apt-get install -y docker-compose-plugin
 
 systemctl enable docker
 systemctl start docker
