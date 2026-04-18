ff8ff6ab034c7f7b68fc13ec18a6825258152390
mike <vatolkinmihajlo@gmail.com>
2026-04-18 23:44:19 +0300

    update mywebapp.sh, move to local

--- scripts/mywebappctl.sh
+++ scripts/mywebappctl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cp mywebappctl /usr/bin/mywebappctl
-chmod +x /usr/bin/mywebappctl
+cp mywebappctl /usr/bin/local/mywebappctl
+chmod +x /usr/local/bin/mywebappctl
 
 echo "MYWEBAPPCTL SCRIPT DONE"
